934010570f9adb1a8c213429a4f64e331a657ade
jacob-ferrell <michaeljacobferrell@gmail.com>
2023-02-11 13:36:34 -0500

    tried to fix issue with base url

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
-import { BrowserRouter as Router } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Router basename="/portfolio">
+    <Router basename='/portfolio'>
       <App />
     </Router>
   </React.StrictMode>,
