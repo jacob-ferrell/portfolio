c759b70a81de3ee6eb79578104b5b27a1941eca2
jacob-ferrell <michaeljacobferrell@gmail.com>
2023-02-27 19:05:54 -0500

    updated project URL's to project.jacob-ferrell.com

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -4,14 +4,13 @@ import bugTracker from "../assets/projectScreenshots/bug-tracker-screenshot2.png
 import cookBook from "../assets/projectScreenshots/cookbook-screenshot.png";
 
 const Projects = (props) => {
-
   const projects = [
     {
       name: "Bug Tracker",
       img: bugTracker,
       description: "A project management app created using the MERN stack",
       repo: "https://github.com/jacob-ferrell/bug-tracker",
-      demo: "https://bug-tracker-rcf6.onrender.com/",
+      demo: "https://bugtracker.jacob-ferrell.com/",
     },
     {
       name: "CookBook",
@@ -19,16 +18,26 @@ const Projects = (props) => {
       description:
         "A recipe book app used for discovering and sharing recipes.  Created using Django",
       repo: "https://github.com/jacob-ferrell/django-recipe-book",
-      demo: "https://cookbook-backend.onrender.com/",
+      demo: "https://cookbook.jacob-ferrell.com/",
     },
   ].map((project) => {
     return (
-      <div id={kebabCase(project.name)} className="project hidden" key={project.name}>
-        <div className="skill-heading">{project.name}</div>
-        <img src={project.img} href={project.demo} alt={project.name + '-thumbnail'}></img>
+      <div
+        id={kebabCase(project.name)}
+        className="project hidden"
+        key={project.name}
+      >
+        <div className="skill-heading">
+          <a className="no-hover" href={project.demo}>
+            {project.name}
+          </a>
+        </div>
+        <a href={project.demo}>
+          <img src={project.img} alt={project.name + "-thumbnail"}></img>
+        </a>
         <div className="project-description">{project.description}</div>
-        <a href={project.repo}>- GitHub Repo -</a>
-        <a href={project.demo}>-Live Demo-</a>
+        <a href={project.repo} className="project-link">-GitHub Repo-</a>
+        <a href={project.demo} className="project-link">-Live Demo-</a>
       </div>
     );
   });
@@ -38,16 +47,14 @@ const Projects = (props) => {
   };
 
   function kebabCase(str) {
-    return str.toLowerCase().replace(/\s/g, '-');
+    return str.toLowerCase().replace(/\s/g, "-");
   }
 
   return (
     <>
       <section id="projects">
         <h1 className="section-heading hidden">Projects</h1>
-        <div className="projects-container">
-          {projects}
-        </div>
+        <div className="projects-container">{projects}</div>
       </section>
     </>
   );
