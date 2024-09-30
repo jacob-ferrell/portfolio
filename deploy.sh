8b7956e487b9e684c20ab4d4aabd7978f69f3157
jacob-ferrell <michaeljacobferrell@gmail.com>
2024-09-30 15:15:13 -0400

    update deploy

--- deploy.sh
+++ deploy.sh
@@ -25,9 +25,9 @@ git add -A
 git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io
-git push -f git@github.com:jacob-ferrell/jacob-ferrell.github.io.git main
+#git push -f git@github.com:jacob-ferrell/jacob-ferrell.github.io.git main
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-#git push -f git@github.com:jacob-ferrell/portfolio.git main:gh-pages
+git push -f git@github.com:jacob-ferrell/portfolio.git main:gh-pages
 
 cd -
